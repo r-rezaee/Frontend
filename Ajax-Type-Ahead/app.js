56fdaafc2146283fe852eebdf3228ab0cbbbeae9
Reza Rezaee <mreza@outlook.com>
2019-02-16 23:28:55 +0100

    first commit

---
+++ Ajax-Type-Ahead/app.js
@@ -0,0 +1,147 @@
+/*const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex)
+    });
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+*/
+
+/*
+const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(url).then(function(response) {
+    return response.json();
+}).then(function(myJson) {
+    cities.push(...myJson);
+});
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        //const regex = new RegExp(wordToMatch, 'gi');
+        //return place.city.startsWith(regex) || place.state.startsWith(regex);
+
+        const word = wordToMatch.toLowerCase();
+
+        return place.city.toLowerCase().startsWith(word) || place.state.toLowerCase().startsWith(word);
+    })
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+        `;
+    }).join('');
+
+    suggestions.innerHTML = html;
+    total.innerHTML = matchArray.length;
+}
+
+
+const total = document.querySelector('.total-match');
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+*/
+
+const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(url).then(function(response) {
+    return response.json();
+}).then(function(myJson) {
+    cities.push(...myJson);
+});
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const word = wordToMatch.toLowerCase();
+        return place.city.toLowerCase().startsWith(word) || place.state.toLowerCase().startsWith(word);
+    });
+}
+
+function numberWithCommas(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+
+        return `
+            <li>
+                <span>${cityName}, ${stateName}</span>
+                <span>${numberWithCommas(place.population)}</span>
+            </li>
+        `;
+
+    }).join('');
+
+    suggestions.innerHTML = html;
+    total.innerHTML = matchArray.length;
+}
+
+const total = document.querySelector('.total-match');
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
