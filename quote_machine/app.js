080011a9342756c01b8944454ad9cec45d722fac
Reza Rezaee <31726186+r-rezaee@users.noreply.github.com>
2019-03-27 16:16:23 +0100

    Update app.js

--- quote_machine/app.js
+++ quote_machine/app.js
@@ -1,21 +1,22 @@
 //cash dom
-let quote_img = document.querySelector(".img-box > img");
-let quote_text = document.querySelector("blockquote > p");
-let quote_author = document.querySelector("cite > span");
+const quote_img = document.querySelector(".img-box > img");
+const quote_text = document.querySelector(".quote");
+const quote_author = document.querySelector(".title");
 
 const new_quote = document.getElementById("getQuote");
 new_quote.addEventListener('click', function(e){
     showQuote();
     e.preventDefault();
 });
-
 const twitt_quote = document.querySelector("#twitt");
+
 
 // the site wont return more than 10 quotes per request
 const url="https://thesimpsonsquoteapi.glitch.me/quotes?count=10";
 
 
 let quotes = [];
+
 
 // the api server won't return more than 10 quotes per request
 // call fetchQuotes function before user reaches the 
@@ -38,18 +39,16 @@ let randNum = 0;
 
 // show the quote
 function showQuote(){
+    quote_img.style.transform = null;
+    quote_img.src = quotes[randNum].image;
     quote_img.alt = quotes[randNum].character;
     quote_text.innerHTML = quotes[randNum].quote;
     quote_author.innerHTML = quotes[randNum].character;
     console.log(quotes[randNum].characterDirection)
     if(quotes[randNum].characterDirection === "Right"){
         quote_img.style.transform = "scaleX(-1)";
-    }else{
-        quote_img.style.transform = null;
     }
-    quote_img.src = quotes[randNum].image;
 
-    
     let tweetUrl = "https://twitter.com/intent/tweet?text=" + quotes[randNum].quote
     + ' - ' + quotes[randNum].character;
 
@@ -63,5 +62,3 @@ function showQuote(){
     }
 
 }
-
-
